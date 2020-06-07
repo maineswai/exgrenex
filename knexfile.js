9039c7450910a4afa73ed1261b1d848b85e958a3
Febrian <solihinfebrian0@gmail.com>
2020-06-07 08:55:32 +0700

    add production to db connection

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,10 @@ module.exports = {
   test: {
     client: "pg",
     connection: "postgres://localhost/test-cjs-web-store" 
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL
   }
 
 };
